d8023dfc1a766c30778cadc091c12e094bc8520e
Dan Crankshaw <dscrankshaw@gmail.com>
2013-12-03 04:35:29 +0000

    Fleshed out wikipedia example.

---
+++ graph/src/main/scala/org/apache/spark/graph/examples/WikiArticle.scala
@@ -0,0 +1,59 @@
+package org.apache.spark.graph.examples
+
+import java.util.regex.Pattern
+import org.apache.spark.graph._
+import java.util.regex.Matcher
+import scala.util.matching.Regex
+import scala.collection.mutable
+import scala.xml._
+import org.apache.spark.serializer.KryoRegistrator
+
+
+
+class WikiArticle(wtext: String) extends Serializable {
+  val links: Array[String] = WikiArticle.parseLinks(wtext)
+  @transient val neighbors = links.map(WikiArticle.titleHash).distinct
+  @transient lazy val redirect: Boolean = !WikiArticle.redirectPattern.findFirstIn(wtext).isEmpty
+  @transient lazy val stub: Boolean = !WikiArticle.stubPattern.findFirstIn(wtext).isEmpty
+  @transient lazy val disambig: Boolean = !WikiArticle.disambigPattern.findFirstIn(wtext).isEmpty
+  val relevant: Boolean = !(redirect || stub || disambig)
+  @transient lazy val tiXML = WikiArticle.titlePattern.findFirstIn(wtext).getOrElse("")
+  val title = XML.loadString(tiXML).text
+  val vertexID: Vid = WikiArticle.titleHash(title)
+  val edges = neighbors.map { n => Edge(vertexID, n, 1.0) }
+}
+
+object WikiArticle {
+  @transient val titlePattern = "<title>(.*)<\\/title>".r
+  @transient val redirectPattern = "#REDIRECT\\s+\\[\\[(.*?)\\]\\]".r
+  @transient val disambigPattern = "\\{\\{disambig\\}\\}".r
+  @transient val stubPattern = "\\-stub\\}\\}".r
+  @transient val linkPattern = Pattern.compile("\\[\\[(.*?)\\]\\]", Pattern.MULTILINE) 
+
+  private def parseLinks(wt: String): Array[String] = {
+    val linkBuilder = new mutable.ArrayBuffer[String]()
+    val matcher: Matcher = linkPattern.matcher(wt)
+    while (matcher.find()) {
+      val temp: Array[String] = matcher.group(1).split("\\|")
+      // println(temp.deep.mkString("\n"))
+      if (temp != null && temp.length > 0) {
+        val link: String = temp(0)
+        if (link.contains(":") == false) {
+          linkBuilder += link
+        }
+      }
+    }
+    return linkBuilder.toArray
+  }
+
+  // substitute underscores for spaces and make lowercase
+  private def canonicalize(title: String): String = {
+    title.trim.toLowerCase.replace(" ", "_")
+  }
+
+  // Hash of the canonical article name. Used for vertex ID.
+  private def titleHash(title: String): Vid = { math.abs(canonicalize(title).hashCode) }
+
+}
+
+
