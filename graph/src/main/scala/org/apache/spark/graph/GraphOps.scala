31dc856af2f9555c2e22fd4eceecd578e61a1c78
Dan Crankshaw <dscrankshaw@gmail.com>
2013-12-07 16:31:20 -0800

    I think coarsen is working but it looks like Graph.triplets is broken

--- graph/src/main/scala/org/apache/spark/graph/GraphOps.scala
+++ graph/src/main/scala/org/apache/spark/graph/GraphOps.scala
@@ -321,8 +321,8 @@ class GraphOps[VD: ClassManifest, ED: ClassManifest](graph: Graph[VD, ED]) {
     // edgesToContract.edges.collect.map(e => println(e))
     // this should return the disjoint set of edges in set B
     val uncontractedEdges = graph.subgraph( {case(et) =>  !epred(et)})
-    println("\nUntouched edges:")
-    uncontractedEdges.edges.collect.map(e => println(e))
+    // println("\nUntouched edges:")
+    // uncontractedEdges.edges.collect.map(e => println(e))
 
     // Connected components graph has Vid as vertex data
     val ccGraph: Graph[Vid,ED] = ConnectedComponents.run(edgesToContract)
@@ -348,7 +348,7 @@ class GraphOps[VD: ClassManifest, ED: ClassManifest](graph: Graph[VD, ED]) {
     // }
 
     val groupedVertices: RDD[(Vid, Seq[EdgeTriplet[(Vid,VD),ED]])] = ccWithDataTriplets.groupBy((t => t.srcAttr._1))
-    groupedVertices.count
+    // groupedVertices.count
     // oldVid -> newVid, newData
     val vertexToContractedVertexMap: RDD[(Vid,(Vid,VD))] = groupedVertices.flatMap { case (ccID, triples) => {
         // Find the new vertex attribute for the contracted vertex
@@ -400,7 +400,7 @@ class GraphOps[VD: ClassManifest, ED: ClassManifest](graph: Graph[VD, ED]) {
     // }
 
     val oldVertices: VertexRDD[VD] = uncontractedEdges.vertices
-    oldVertices.count
+    // oldVertices.count
 
     // the vertices that were part of being contracted and thus have new vertex IDs
     val updatedVertices: VertexRDD[(Vid, VD)] = VertexRDD(vertexToContractedVertexMap)
@@ -428,18 +428,18 @@ class GraphOps[VD: ClassManifest, ED: ClassManifest](graph: Graph[VD, ED]) {
       edgesToUpdate: RDD[Edge[ED]]): RDD[Edge[ED]] = {
 
       val srcRDD: RDD[(Vid,(Vid,Vid))] = edgesToUpdate.map { e => (e.srcId, (e.srcId, e.dstId)) }
-      srcRDD.count
-      println("srcRDD")
-      srcRDD.collect.map(e => println(e))
+      // srcRDD.count
+      // println("srcRDD")
+      // srcRDD.collect.map(e => println(e))
       val dstRDD: RDD[(Vid,(Vid,Vid))] = edgesToUpdate.map { e => (e.dstId, (e.srcId, e.dstId)) }
-      dstRDD.count
-      println("dstRDD")
-      dstRDD.collect.map(e => println(e))
+      // dstRDD.count
+      // println("dstRDD")
+      // dstRDD.collect.map(e => println(e))
       val dataRDD: RDD[((Vid, Vid), ED)] = edgesToUpdate.map { e => ((e.srcId, e.dstId), e.attr) }
-      dataRDD.count
-      println("dataRDD")
-      dataRDD.collect.map(e=> println(e))
-      println("done")
+      // dataRDD.count
+      // println("dataRDD")
+      // dataRDD.collect.map(e=> println(e))
+      // println("done")
 
       // once again we can do leftZipJoins because it's okay if there are unmatched entries in updatedVertexIDs
       // if there 
@@ -455,28 +455,35 @@ class GraphOps[VD: ClassManifest, ED: ClassManifest](graph: Graph[VD, ED]) {
 
       val newSrcVids: RDD[((Vid, Vid), Vid)] = srcRDD.leftOuterJoin(updatedVertexIDs)
         .map { case(oldId: Vid, (srcAndDst: (Vid, Vid), newId: Option[Vid])) => (srcAndDst, newId.getOrElse(oldId))}
-      println("newSrcVids:")
-      newSrcVids.collect.map(a => println(a))
+      // println("newSrcVids:")
+      // newSrcVids.collect.map(a => println(a))
 
 
       val newDstVids: RDD[((Vid, Vid), Vid)] = dstRDD.leftOuterJoin(updatedVertexIDs)
         .map { case(oldId: Vid, (srcAndDst: (Vid, Vid), newId: Option[Vid])) => (srcAndDst, newId.getOrElse(oldId))}
 
-      
-    //   val newEdges: RDD[Edge[ED]] = newSrcVids.join(newDstVids).join(dataRDD)
-    //     .map { case (_: (Vid,Vid), ((newSrc: Vid, newDst: Vid), data: ED)) => Edge(newSrc, newDst, data)}
-    //   newEdges
-    // }
+
+      // val newEdges: RDD[Edge[ED]] = newSrcVids.join(newDstVids).join(dataRDD)
+      //   .map { case (_: (Vid,Vid), ((newSrc: Vid, newDst: Vid), data: ED)) => Edge(newSrc, newDst, data)}
+
+        
+      val srcJoinedDst: RDD[((Vid,Vid), (Vid, Vid))] = newSrcVids.join(newDstVids)
+      val srcJoinedDstJoinedData: RDD[((Vid,Vid), ((Vid,Vid),ED))] = srcJoinedDst.join(dataRDD)
 
 
-      val newEdges: RDD[Edge[ED]] = newSrcVids.join(newDstVids).join(dataRDD)
-        .map { case (_: (Vid,Vid), ((newSrc: Vid, newDst: Vid), data: ED)) => Edge(newSrc, newDst, data)}
+      val newEdges: RDD[Edge[ED]] = srcJoinedDstJoinedData
+        .map { case ((_: Vid, _: Vid), ((newSrc: Vid, newDst: Vid), data: ED)) => Edge(newSrc, newDst, data)}
+        // .map { case (_: (Vid,Vid), ((newSrc: Vid, newDst: Vid), data: ED)) => Edge(newSrc, newDst, data)}
+
+      // println("\nNewEdges")
+      // newEdges.collect.map(a => println(a))
+      // println("\n\n")
       newEdges
     }
 
-
+    // filter out self-loops
     val newEdges: RDD[Edge[ED]] = updateETable(updatedVertexIDs, uncontractedEdges.edges)
-    newEdges.count
+    // newEdges.count
     val filteredEdges = newEdges.filter(e => e.srcId != e.dstId)
 
     Graph(newVertices, filteredEdges)
